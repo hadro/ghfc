d83936e1ffdf12c28855403eb4192390af1b309a
hadro <josh.hadro@gmail.com>
2020-07-26 20:02:10 +0000

    tweak

--- recurring-reports/log_aggregation_jsonl.sh
+++ recurring-reports/log_aggregation_jsonl.sh
@@ -1,13 +1,15 @@
 today=`date +%Y-%m-%d`
 echo $today
 
+cd /home/hadro/ghfc/recurring-reports/
+
 # Download the log files from the GHFC server
 echo "Pulling log files..."
-cp /home/christopher/check-in-stats/shop_log.jsonl ./logs/
+cp /home/christopher/check-in-stats/shop_log.jsonl /home/hadro/ghfc/recurring-reports/logs/shop_log.jsonl
 
 # Use some pandas to extract stuff we know we'll need
 echo "Processing logs into log.csv..."
-python3 process_logs_jsonl.py
+python3 /home/hadro/ghfc/recurring-reports/process_logs_jsonl.py
 
 echo "Running RMarkdown script..."
 #Rscript -e "rmarkdown::render('report.Rmd', 
@@ -15,8 +17,8 @@ echo "Running RMarkdown script..."
 #				'.html', sep=''
 #				))"
 
-Rscript -e "rmarkdown::render('report.Rmd',
-                  output_file = paste('./reports/report_', Sys.Date(), 
+Rscript -e "rmarkdown::render('/home/hadro/ghfc/recurring-reports/report.Rmd',
+                  output_file = paste('/home/hadro/ghfc/recurring-reports/reports/report_', Sys.Date(), 
                                       '.html', sep=''))"
 
 
@@ -27,7 +29,7 @@ Rscript -e "rmarkdown::render('report.Rmd',
 #echo "Sending the report for" $today | mailx -s "GHFC Report for $today" -A ./report_$today.html josh.hadro@gmail.com
 
 # Push to git repo
-git add ./reports/report_$today.html
+git add /home/hadro/ghfc/recurring-reports/reports/report_$today.html
 git commit -m "Add report for $today"
 git push
 
